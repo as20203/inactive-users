17c92e666730800c965575382ccd73441488c84c
as20203 <jzaheer.bese16seecs@seecs.edu.pk>
2021-03-16 11:49:45 +0500

    Adds user inactivity classification function.

--- inactive.js
+++ inactive.js
@@ -19,6 +19,15 @@ const inactiveThirdStage = () => {
 
 
 // this is the main method that acts like a worker and runs every 1 minute
+//** This function is running every second rather than after every minute. */
 setInterval(function(){
     // please write your code here
+    const { lastActivity } = user;
+    const currentTime = new Date().getTime();
+    // Finding elapsed minutes
+    // Converting the miliseconds to minutes
+    const elapsedTime = ((currentTime - lastActivity) / (1000 * 60)) % 5;
+    if (elapsedTime < 2) inactiveFirstStage()
+    else if (elapsedTime < 4) inactiveSecondStage()
+    else inactiveThirdStage()
 }, 1000);
